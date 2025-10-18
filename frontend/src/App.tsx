d29f39c94f8e4c6e3ece15c019a2a74620460823
JBardoe <jackbardoe@gmail.com>
2025-10-18 11:31:43 +0100

    Added pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<Router>
+			<Routes>
+				<Route path="*" element={<Home />} />
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
