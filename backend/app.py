5e15335811eb8e87d90b71fb326591668f9995e9
JBardoe <jackbardoe@gmail.com>
2025-10-18 11:49:24 +0100

    Initialised backend

---
+++ backend/app.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask, jsonify, send_from_directory
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, static_folder="build", static_url_path="/")
+app.secret_key = "secretKey"
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///whack.sqlite'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db.init_app(app)
+
+CORS(app, supports_credentials=True)
+
+
+@app.errorhandler(Exception)
+def handle_error(e):
+	print("Error:", e)
+	return jsonify(message="An unexpected error has occurred"), 500
+
+
+@app.route('/')
+@app.route('/<path:path>')
+def serve_react(path=''):
+	if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+		return send_from_directory(app.static_folder, path)
+	else:
+		return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
